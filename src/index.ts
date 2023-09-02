f7f02e4aac6e6cde79c478dc0943740f6d3dad04
Nick Petalas <webmasternikos@gmail.com>
2023-09-03 00:03:47 +0100

    allow importing from json files

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import "./styles.css";
 
 import { default as init } from "./assets/wasm/renderer";
 
+import test from "./assets/test.json";
+
 init().then(() => {
   console.log("WASM Loaded");
+  console.log(test);
 });
